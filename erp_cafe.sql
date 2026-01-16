c4d9642737261c35ea2571e7abfaaa338a37537f
adi3947 <adi3947@gmail.com>
2026-01-16 16:57:49 +0700

    update database erp_cafe.sql

--- erp_cafe.sql
+++ erp_cafe.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 01, 2026 at 03:49 AM
+-- Generation Time: Jan 16, 2026 at 09:44 AM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.20
 
@@ -42,7 +42,7 @@ CREATE TABLE `attendances` (
 
 INSERT INTO `attendances` (`id`, `employee_id`, `date`, `time_in`, `time_out`, `status`, `location`) VALUES
 (1, 2, '2026-01-01', '01:34:00', '01:34:00', 'Hadir', 'Bekasi'),
-(2, 7, '2026-01-01', '03:31:00', '09:31:00', 'Hadir', 'Jakarta');
+(3, 1, '2026-01-01', '08:24:00', '08:24:00', 'Hadir', 'Bekasi');
 
 -- --------------------------------------------------------
 
@@ -62,7 +62,8 @@ CREATE TABLE `ci_sessions` (
 --
 
 INSERT INTO `ci_sessions` (`id`, `ip_address`, `timestamp`, `data`) VALUES
-('asiti1vvlguklosk8kp04eemqs5m6trk', '::1', 1767235284, 0x5f5f63695f6c6173745f726567656e65726174657c693a313736373233353030343b757365725f69647c733a313a2231223b6e616d657c733a31333a2241646d696e6973747261746f72223b656d61696c7c733a31343a2261646d696e406d61696c2e636f6d223b726f6c657c733a31333a2261646d696e6973747261746f72223b6c6f676765645f696e7c623a313b);
+('08on2ivr9ttm69jd8fep41bvlbcogfr9', '::1', 1768487126, 0x5f5f63695f6c6173745f726567656e65726174657c693a313736383438373035383b757365725f69647c733a313a2239223b6e616d657c733a31343a22496d656c64612052616b6120432e223b656d61696c7c733a31383a22696d656c64612e7263406d61696c2e636f6d223b726f6c657c733a353a226f776e6572223b6c6f676765645f696e7c623a313b),
+('5jvm5n23jqq6n862p8uc637vuj92b3ts', '::1', 1767356898, 0x5f5f63695f6c6173745f726567656e65726174657c693a313736373335363739323b757365725f69647c733a313a2231223b6e616d657c733a31333a2241646d696e6973747261746f72223b656d61696c7c733a31343a2261646d696e406d61696c2e636f6d223b726f6c657c733a31333a2261646d696e6973747261746f72223b6c6f676765645f696e7c623a313b);
 
 -- --------------------------------------------------------
 
@@ -93,6 +94,215 @@ INSERT INTO `employees` (`id`, `user_id`, `nik`, `name`, `join_date`, `position`
 (5, NULL, 'A.0005', 'Ahmad Fauzi', '2026-01-01', 'Barista', 3500000, 'Aktif'),
 (6, NULL, 'S.0006', 'Siti Aisyah', '2026-01-01', 'Kasir', 3200000, 'Aktif'),
 (7, NULL, 'B.0007', 'Budi Santoso', '2026-01-01', 'Supervisor', 5000000, 'Aktif');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permissions`
+--
+
+CREATE TABLE `permissions` (
+  `id` int(11) NOT NULL,
+  `role` varchar(50) NOT NULL,
+  `module` varchar(50) NOT NULL,
+  `can_view` tinyint(1) DEFAULT '0',
+  `can_create` tinyint(1) DEFAULT '0',
+  `can_edit` tinyint(1) DEFAULT '0',
+  `can_delete` tinyint(1) DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `permissions`
+--
+
+INSERT INTO `permissions` (`id`, `role`, `module`, `can_view`, `can_create`, `can_edit`, `can_delete`, `created_at`) VALUES
+(1, 'administrator', 'dashboard', 1, 1, 1, 1, '2026-01-16 07:39:24'),
+(2, 'administrator', 'hr', 1, 1, 1, 1, '2026-01-16 07:39:24'),
+(3, 'administrator', 'sales', 1, 1, 1, 1, '2026-01-16 07:39:24'),
+(4, 'administrator', 'purchasing', 1, 1, 1, 1, '2026-01-16 07:39:24'),
+(5, 'administrator', 'inventory', 1, 1, 1, 1, '2026-01-16 07:39:24'),
+(6, 'administrator', 'users', 1, 1, 1, 1, '2026-01-16 07:39:24'),
+(7, 'administrator', 'reports', 1, 1, 1, 1, '2026-01-16 07:39:24'),
+(15, 'asasaa', 'dashboard', 0, 0, 0, 0, '2026-01-16 07:41:02'),
+(16, 'asasaa', 'hr', 0, 0, 0, 0, '2026-01-16 07:41:02'),
+(17, 'asasaa', 'sales', 0, 0, 0, 0, '2026-01-16 07:41:02'),
+(18, 'asasaa', 'purchasing', 0, 0, 0, 0, '2026-01-16 07:41:02'),
+(19, 'asasaa', 'inventory', 0, 0, 0, 0, '2026-01-16 07:41:02'),
+(20, 'asasaa', 'users', 0, 0, 0, 0, '2026-01-16 07:41:02'),
+(21, 'asasaa', 'reports', 0, 0, 0, 0, '2026-01-16 07:41:02'),
+(22, 'cashier', 'dashboard', 0, 0, 0, 0, '2026-01-16 07:51:23'),
+(23, 'cashier', 'hr', 0, 0, 0, 0, '2026-01-16 07:51:23'),
+(24, 'cashier', 'sales', 0, 0, 0, 0, '2026-01-16 07:51:23'),
+(25, 'cashier', 'purchasing', 0, 0, 0, 0, '2026-01-16 07:51:23'),
+(26, 'cashier', 'inventory', 0, 0, 0, 0, '2026-01-16 07:51:23'),
+(27, 'cashier', 'users', 0, 0, 0, 0, '2026-01-16 07:51:23'),
+(28, 'cashier', 'reports', 0, 0, 0, 0, '2026-01-16 07:51:23'),
+(36, 'owner', 'dashboard', 1, 1, 1, 1, '2026-01-16 08:06:47'),
+(37, 'owner', 'hr', 1, 1, 1, 1, '2026-01-16 08:06:47'),
+(38, 'owner', 'sales', 1, 1, 1, 1, '2026-01-16 08:06:47'),
+(39, 'owner', 'purchasing', 1, 1, 1, 1, '2026-01-16 08:06:47'),
+(40, 'owner', 'inventory', 1, 1, 1, 1, '2026-01-16 08:06:47'),
+(41, 'owner', 'users', 1, 1, 1, 1, '2026-01-16 08:06:47'),
+(42, 'owner', 'reports', 1, 1, 1, 1, '2026-01-16 08:06:47'),
+(50, 'hr_manager', 'dashboard', 1, 0, 0, 0, '2026-01-16 08:06:47'),
+(51, 'hr_manager', 'hr', 1, 1, 1, 1, '2026-01-16 08:06:47'),
+(52, 'hr_manager', 'sales', 0, 0, 0, 0, '2026-01-16 08:06:47'),
+(53, 'hr_manager', 'purchasing', 0, 0, 0, 0, '2026-01-16 08:06:47'),
+(54, 'hr_manager', 'inventory', 0, 0, 0, 0, '2026-01-16 08:06:47'),
+(55, 'hr_manager', 'users', 0, 0, 0, 0, '2026-01-16 08:06:47'),
+(56, 'hr_manager', 'reports', 1, 0, 0, 0, '2026-01-16 08:06:47'),
+(57, 'sales_manager', 'dashboard', 1, 0, 0, 0, '2026-01-16 08:06:47'),
+(58, 'sales_manager', 'hr', 0, 0, 0, 0, '2026-01-16 08:06:47'),
+(59, 'sales_manager', 'sales', 1, 1, 1, 1, '2026-01-16 08:06:47'),
+(60, 'sales_manager', 'purchasing', 0, 0, 0, 0, '2026-01-16 08:06:47'),
+(61, 'sales_manager', 'inventory', 1, 0, 0, 0, '2026-01-16 08:06:47'),
+(62, 'sales_manager', 'users', 0, 0, 0, 0, '2026-01-16 08:06:47'),
+(63, 'sales_manager', 'reports', 1, 0, 0, 0, '2026-01-16 08:06:47'),
+(64, 'purchase_manager', 'dashboard', 1, 0, 0, 0, '2026-01-16 08:06:47'),
+(65, 'purchase_manager', 'hr', 0, 0, 0, 0, '2026-01-16 08:06:47'),
+(66, 'purchase_manager', 'sales', 0, 0, 0, 0, '2026-01-16 08:06:47'),
+(67, 'purchase_manager', 'purchasing', 1, 1, 1, 1, '2026-01-16 08:06:47'),
+(68, 'purchase_manager', 'inventory', 1, 0, 0, 0, '2026-01-16 08:06:47'),
+(69, 'purchase_manager', 'users', 0, 0, 0, 0, '2026-01-16 08:06:47'),
+(70, 'purchase_manager', 'reports', 1, 0, 0, 0, '2026-01-16 08:06:47'),
+(71, 'inventory_manager', 'dashboard', 1, 0, 0, 0, '2026-01-16 08:06:47'),
+(72, 'inventory_manager', 'hr', 0, 0, 0, 0, '2026-01-16 08:06:47'),
+(73, 'inventory_manager', 'sales', 1, 0, 0, 0, '2026-01-16 08:06:47'),
+(74, 'inventory_manager', 'purchasing', 1, 0, 0, 0, '2026-01-16 08:06:47'),
+(75, 'inventory_manager', 'inventory', 1, 1, 1, 1, '2026-01-16 08:06:47'),
+(76, 'inventory_manager', 'users', 0, 0, 0, 0, '2026-01-16 08:06:47'),
+(77, 'inventory_manager', 'reports', 1, 0, 0, 0, '2026-01-16 08:06:47');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `sku` varchar(50) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `unit` varchar(20) NOT NULL,
+  `min_stock` int(11) DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `sku`, `name`, `category_id`, `unit`, `min_stock`, `created_at`) VALUES
+(1, 'DRK-001', 'Kopi Hitam', 1, 'gelas', 20, '2026-01-01 08:58:09'),
+(2, 'DRK-002', 'Es Teh Manis', 1, 'gelas', 30, '2026-01-01 08:58:09'),
+(3, 'DRK-003', 'Cappuccino', 1, 'gelas', 15, '2026-01-01 08:58:09'),
+(4, 'DRK-004', 'Latte', 1, 'gelas', 15, '2026-01-01 08:58:09'),
+(5, 'DRK-005', 'Air Mineral Botol', 1, 'botol', 25, '2026-01-01 08:58:09'),
+(6, 'FD-001', 'Nasi Goreng', 2, 'porsi', 10, '2026-01-01 08:58:09'),
+(7, 'FD-002', 'Mie Goreng', 2, 'porsi', 10, '2026-01-01 08:58:09'),
+(8, 'FD-003', 'Ayam Goreng', 2, 'porsi', 8, '2026-01-01 08:58:09'),
+(9, 'FD-004', 'Burger Beef', 2, 'porsi', 7, '2026-01-01 08:58:09'),
+(10, 'FD-005', 'Kentang Goreng', 2, 'porsi', 12, '2026-01-01 08:58:09'),
+(11, 'SNK-001', 'Donat', 3, 'pcs', 20, '2026-01-01 08:58:09'),
+(12, 'SNK-002', 'Roti Bakar', 3, 'porsi', 15, '2026-01-01 08:58:09'),
+(13, 'SNK-003', 'Pisang Goreng', 3, 'porsi', 15, '2026-01-01 08:58:09'),
+(14, 'SNK-004', 'Keripik Kentang', 3, 'pcs', 25, '2026-01-01 08:58:09'),
+(15, 'SNK-005', 'Kacang Goreng', 3, 'pcs', 20, '2026-01-01 08:58:09');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_stocks`
+--
+
+CREATE TABLE `product_stocks` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `warehouse_id` int(11) NOT NULL,
+  `qty` int(11) NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `product_stocks`
+--
+
+INSERT INTO `product_stocks` (`id`, `product_id`, `warehouse_id`, `qty`, `created_at`) VALUES
+(1, 1, 1, 50, '2026-01-01 08:58:09'),
+(2, 2, 1, 60, '2026-01-01 08:58:09'),
+(3, 3, 1, 40, '2026-01-01 08:58:09'),
+(4, 4, 1, 40, '2026-01-01 08:58:09'),
+(5, 5, 1, 70, '2026-01-01 08:58:09'),
+(6, 6, 1, 30, '2026-01-01 08:58:09'),
+(7, 7, 1, 30, '2026-01-01 08:58:09'),
+(8, 8, 1, 25, '2026-01-01 08:58:09'),
+(9, 9, 1, 20, '2026-01-01 08:58:09'),
+(10, 10, 1, 35, '2026-01-01 08:58:09'),
+(11, 11, 1, 40, '2026-01-01 08:58:09'),
+(12, 12, 1, 30, '2026-01-01 08:58:09'),
+(13, 13, 1, 30, '2026-01-01 08:58:09'),
+(14, 14, 1, 50, '2026-01-01 08:58:09'),
+(15, 15, 1, 45, '2026-01-01 08:58:09'),
+(16, 1, 1, 50, '2026-01-01 08:58:39'),
+(17, 2, 1, 60, '2026-01-01 08:58:39'),
+(18, 3, 1, 40, '2026-01-01 08:58:39'),
+(19, 4, 1, 40, '2026-01-01 08:58:39'),
+(20, 5, 1, 70, '2026-01-01 08:58:39'),
+(21, 6, 1, 30, '2026-01-01 08:58:39'),
+(22, 7, 1, 30, '2026-01-01 08:58:39'),
+(23, 8, 1, 25, '2026-01-01 08:58:39'),
+(24, 9, 1, 20, '2026-01-01 08:58:39'),
+(25, 10, 1, 35, '2026-01-01 08:58:39'),
+(26, 11, 1, 40, '2026-01-01 08:58:39'),
+(27, 12, 1, 30, '2026-01-01 08:58:39'),
+(28, 13, 1, 30, '2026-01-01 08:58:39'),
+(29, 14, 1, 50, '2026-01-01 08:58:39'),
+(30, 15, 1, 45, '2026-01-01 08:58:39'),
+(31, 1, 1, 50, '2026-01-01 08:58:39'),
+(32, 2, 1, 60, '2026-01-01 08:58:39'),
+(33, 3, 1, 40, '2026-01-01 08:58:39'),
+(34, 4, 1, 40, '2026-01-01 08:58:39'),
+(35, 5, 1, 70, '2026-01-01 08:58:39'),
+(36, 6, 1, 30, '2026-01-01 08:58:39'),
+(37, 7, 1, 30, '2026-01-01 08:58:39'),
+(38, 8, 1, 25, '2026-01-01 08:58:39'),
+(39, 9, 1, 20, '2026-01-01 08:58:39'),
+(40, 10, 1, 35, '2026-01-01 08:58:39'),
+(41, 11, 1, 40, '2026-01-01 08:58:39'),
+(42, 12, 1, 30, '2026-01-01 08:58:39'),
+(43, 13, 1, 30, '2026-01-01 08:58:39'),
+(44, 14, 1, 50, '2026-01-01 08:58:39'),
+(45, 15, 1, 45, '2026-01-01 08:58:39');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `role_name` varchar(50) NOT NULL,
+  `display_name` varchar(100) NOT NULL,
+  `description` text,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `role_name`, `display_name`, `description`, `created_at`) VALUES
+(1, 'administrator', 'Administrator', '', '2026-01-16 04:30:21'),
+(2, 'sales', 'Sales', '', '2026-01-16 06:13:51'),
+(4, 'owner', 'Owner', 'Pemilik bisnis', '2026-01-16 07:22:54'),
+(5, 'cashier', 'Kasir', 'Kasir toko', '2026-01-16 07:22:54'),
+(6, 'hr_manager', 'HR Manager', 'Manager HR', '2026-01-16 07:22:54'),
+(7, 'sales_manager', 'Sales Manager', 'Manager Penjualan', '2026-01-16 07:22:54'),
+(8, 'purchase_manager', 'Purchase Manager', 'Manager Pembelian', '2026-01-16 07:22:54'),
+(9, 'inventory_manager', 'Inventory Manager', 'Manager Inventori', '2026-01-16 07:22:54');
 
 -- --------------------------------------------------------
 
@@ -151,6 +361,34 @@ INSERT INTO `salaries` (`id`, `employee_id`, `period_month`, `period_year`, `bas
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `stock_mutations`
+--
+
+CREATE TABLE `stock_mutations` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `type` enum('in','out','adjust') NOT NULL,
+  `ref_id` varchar(50) DEFAULT NULL,
+  `note` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `stock_mutations`
+--
+
+INSERT INTO `stock_mutations` (`id`, `product_id`, `qty`, `type`, `ref_id`, `note`, `created_at`) VALUES
+(1, 1, 2, 'out', 'INV-001', 'Penjualan kopi', '2026-01-01 08:58:39'),
+(2, 11, 3, 'out', 'INV-002', 'Penjualan snack', '2026-01-01 08:58:39'),
+(3, 5, -5, 'adjust', 'ADJ-001', 'Stok rusak', '2026-01-01 08:58:39'),
+(4, 1, 2, 'out', 'INV-001', 'Penjualan kopi', '2026-01-01 08:58:39'),
+(5, 11, 3, 'out', 'INV-002', 'Penjualan snack', '2026-01-01 08:58:39'),
+(6, 5, -5, 'adjust', 'ADJ-001', 'Stok rusak', '2026-01-01 08:58:39');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -171,9 +409,10 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `remember_expire`, `role`, `created_at`) VALUES
 (1, 'Administrator', 'admin@mail.com', '$2y$10$nTqjkwl6ZBB9ngSwaDXQs.LYiLh1VNssXW4D5pFeWwPxYAC8sCSMq', NULL, NULL, 'administrator', '2025-10-31 10:55:19'),
-(3, 'ADI DWI SAPUTRA', 'adi3947@gmail.com', '$2y$10$Aexn66EnSL0L9JxvVdQO.e0pOvVg2uilVg/ABznQfhmLaOIdtOKQy', NULL, NULL, 'admin', '2025-10-30 14:47:49'),
-(6, 'coba1', 'coba@mail.com', '$2y$10$94iA7XVvpRjvyX18C5y/Qes4xvMQh6G141lauSJwCg28f5QDQfgO6', NULL, NULL, 'admin', '2025-11-01 14:31:35'),
-(8, '123', '123@mail.com', '$2y$10$AN8f2w8g9W14jeomvr1C3eAxhrO/FjBVZ/0aiu.kayRci6vWFZg0S', NULL, NULL, 'admin', '2025-11-01 14:56:54');
+(3, 'Adi Dwi S', 'adi3947@gmail.com', '$2y$10$Aexn66EnSL0L9JxvVdQO.e0pOvVg2uilVg/ABznQfhmLaOIdtOKQy', NULL, NULL, 'owner', '2025-10-30 14:47:49'),
+(9, 'Imelda Raka C.', 'imelda.rc@mail.com', '$2y$10$B/Nzr0PZq0KHJYhyRTMnLuIUoNFPUBw3xpIJHOsqB9bc.IuqqCvUK', NULL, NULL, 'owner', '2026-01-15 15:15:30'),
+(10, 'Siti Aisyah', 'siti@mail.com', '$2y$10$2MYsPN/05BLJYgjnnp8QG.Wrexlm3L4sk0L1YxRbVxe3pEuZRMM3G', NULL, NULL, 'cashier', '2026-01-15 15:17:12'),
+(19, '123456', '123456@mail.com', '$2y$10$lE39mTrKu1v7hXG2tZQ1IOi0r1dakYHhdZY3mHI8unQi00PK4LK6a', NULL, NULL, 'hr_manager', '2026-01-16 09:17:07');
 
 --
 -- Indexes for dumped tables
@@ -199,11 +438,46 @@ ALTER TABLE `employees`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `permissions`
+--
+ALTER TABLE `permissions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_role_module` (`role`,`module`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `sku` (`sku`);
+
+--
+-- Indexes for table `product_stocks`
+--
+ALTER TABLE `product_stocks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_stock_product` (`product_id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `role_name` (`role_name`);
+
+--
 -- Indexes for table `salaries`
 --
 ALTER TABLE `salaries`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `uniq_salary_period` (`employee_id`,`period_month`,`period_year`);
+
+--
+-- Indexes for table `stock_mutations`
+--
+ALTER TABLE `stock_mutations`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_mutation_product` (`product_id`);
 
 --
 -- Indexes for table `users`
@@ -220,22 +494,63 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `attendances`
 --
 ALTER TABLE `attendances`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `employees`
 --
 ALTER TABLE `employees`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `permissions`
+--
+ALTER TABLE `permissions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=127;
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+--
+-- AUTO_INCREMENT for table `product_stocks`
+--
+ALTER TABLE `product_stocks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 --
 -- AUTO_INCREMENT for table `salaries`
 --
 ALTER TABLE `salaries`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 --
+-- AUTO_INCREMENT for table `stock_mutations`
+--
+ALTER TABLE `stock_mutations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `product_stocks`
+--
+ALTER TABLE `product_stocks`
+  ADD CONSTRAINT `fk_stock_product` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `stock_mutations`
+--
+ALTER TABLE `stock_mutations`
+  ADD CONSTRAINT `fk_mutation_product` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
